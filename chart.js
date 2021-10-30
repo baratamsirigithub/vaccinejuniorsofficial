5afa3404a25e36b4eb0db74003a3bca7e05ea9e8
baratamsirigithub <88707992+baratamsirigithub@users.noreply.github.com>
2021-10-30 10:40:46 +0400

    Add files via upload

---
+++ chart.js
@@ -0,0 +1,52 @@
+// const CHART = document.getElementById("lineChart");
+// CHART.isDefaultNamespace.scale.ticks.beginAtZero = true;
+
+// let barChart = new CHART(CHART, {
+//     type: 'pie',
+//     data: {
+//         labels: ['Strength','Skill','Health','Speed','Luck'],
+//         datasets: [
+//             {
+//                 label: 'Points',
+//                 backgroundColor: ['#f1c40f','#e67e22','#16a085','#2980b9'
+//                 data: [10,20,55,30,10]
+//             }
+//         ]
+//     }
+// });
+
+// const config = {
+//     type: 'doughnut',
+//     data: data,
+//     options: {
+//       responsive: true,
+//       plugins: {
+//         legend: {
+//           position: 'top',
+//         },
+//         title: {
+//           display: true,
+//           text: 'Chart.js Doughnut Chart'
+//         }
+//       }
+//     },
+//   };
+
+//   const DATA_COUNT = 5;
+// const NUMBER_CFG = {count: DATA_COUNT, min: 0, max: 100};
+
+// const data = {
+//   labels: ['Red', 'Orange', 'Yellow', 'Green', 'Blue'],
+//   datasets: [
+//     {
+//       label: 'Dataset 1',
+//       data: Utils.numbers(NUMBER_CFG),
+//       backgroundColor: Object.values(Utils.CHART_COLORS),
+//     }
+//   ]
+// };
+
+
+
+
+
